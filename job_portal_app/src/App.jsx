d1591cf9c68b372a62c37f0ef005c3fdcdb2c89c
Xarvis <nsnishant51@gmail.com>
2024-06-23 19:32:50 +0530

    fixed jobs UI

--- job_portal_app/src/App.jsx
+++ job_portal_app/src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+    Route,
+    createBrowserRouter,
+    createRoutesFromElements,
+    RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    // Add New Job
+    const addJob = async (newJob) => {
+        const res = await fetch("/api/jobs", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newJob),
+        });
+        return;
+    };
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    // Delete Job
+    const deleteJob = async (id) => {
+        const res = await fetch(`/api/jobs/${id}`, {
+            method: "DELETE",
+        });
+        return;
+    };
 
-export default App
+    // Update Job
+    const updateJob = async (job) => {
+        const res = await fetch(`/api/jobs/${job.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(job),
+        });
+        return;
+    };
+
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route path="/" element={<MainLayout />}>
+                <Route index element={<HomePage />} />
+                <Route path="/jobs" element={<JobsPage />} />
+                <Route
+                    path="/add-job"
+                    element={<AddJobPage addJobSubmit={addJob} />}
+                />
+                <Route
+                    path="/edit-job/:id"
+                    element={<EditJobPage updateJobSubmit={updateJob} />}
+                    loader={jobLoader}
+                />
+                <Route
+                    path="/jobs/:id"
+                    element={<JobPage deleteJob={deleteJob} />}
+                    loader={jobLoader}
+                />
+                <Route path="*" element={<NotFoundPage />} />
+            </Route>
+        )
+    );
+
+    return <RouterProvider router={router} />;
+};
+export default App;
